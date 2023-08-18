66224b5633044720d197c1b078b8425fcaf0f11c
baggsviva <124136045+baggsviva@users.noreply.github.com>
2023-08-18 16:19:31 +0800

    Add files via upload

---
+++ components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Main() {
+  return (
+    <p>
+      In life, there are so many crossroads, and there are many things we have
+      to consider, such as shifting or developing new career-level skill sets
+      for which we may not yet have all the necessary tools. With the entire
+      planet Earth currently in chaos due to the COVID-19 pandemic, many people
+      are struggling and seeking alternatives to face this challenge and ensure
+      their survival. Remote online jobs have emerged as a viable solution,
+      particularly for those with IT skills
+    </p>
+  );
+}
+
+export default Main;
